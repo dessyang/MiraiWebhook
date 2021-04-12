d4e376214e8b68d4ad65ac73ad1979fdb30326e1
yjymh <814123650@qq.com>
2021-04-12 10:11:50 +0800

    first

---
+++ src/main/java/com/yjymh/miraiwebhook/MiraiWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.yjymh.miraiwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraiWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiraiWebhookApplication.class, args);
+    }
+
+}
