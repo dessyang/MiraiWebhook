3fa349993037fe5ef7f22fbbf56f9e964f5b6b87
yjymh <814123650@qq.com>
2021-04-17 21:32:29 +0800

    去除Lombok 提取token公共类

---
+++ src/main/java/com/yjymh/miraiwebhook/entity/GroupToken.java
@@ -0,0 +1,18 @@
+package com.yjymh.miraiwebhook.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupToken {
+    private Long account;
+    private String token;
+    private boolean status;
+    private Date update_time;
+    private Date create_time;
+}
--- src/main/java/com/yjymh/miraiwebhook/entity/Response.java
+++ src/main/java/com/yjymh/miraiwebhook/entity/Response.java
@@ -1,12 +1,33 @@
 package com.yjymh.miraiwebhook.entity;
 
-import lombok.*;
 
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Response {
+
     private int code;
     private String msg;
+
+    public Response(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Response() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
 }
